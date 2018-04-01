cecf884f0a2e3119cb19afcc2bb1d0765ad084ea
Rojaya <rmaharjan@apple.com>
2018-04-01 14:48:32 -0700

    Second sets of update

--- AppleProject/src/Main.java
+++ AppleProject/src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
 	// add a comment
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("I would to print something");
 	}
-	
+
 }
