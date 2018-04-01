606227c4ab5e76dba67eb075c525344e2c785518
Rojaya <rmaharjan@apple.com>
2018-04-01 15:41:09 -0700

    A function has been added

--- AppleProject/src/Main.java
+++ AppleProject/src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 		System.out.println("Hello World");
 		System.out.println("I would to print something");
 		// There is some more changes that i would like to make to this project.
+		
+	}
+	public static void Name() {
+		System.out.println("A new function has been added");
 	}
 
 }
