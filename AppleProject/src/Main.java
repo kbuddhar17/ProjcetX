74e289c80707da65efeeec05b03341e4843ec6bf
Rojaya <rmaharjan@apple.com>
2018-04-01 15:37:45 -0700

    initial commit

--- AppleProject/src/Main.java
+++ AppleProject/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I would to print something");
+		// There is some more changes that i would like to make to this project.
 	}
 
 }
